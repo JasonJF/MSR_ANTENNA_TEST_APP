c4e9c538dfb19ef3d53eeb60fcb7347e2560fc89
jasonjf <jasonf@reutech.co.za>
2019-05-27 17:28:07 +0200

    Reshuffled UI, added table

--- web/js/testing.js
+++ web/js/testing.js
@@ -3,3 +3,6 @@ document.getElementById("logBox").disabled = true; //make textarea disabled
 function uiReload() {
     location.reload(true)
     }      //relaods the current page
+
+function populateTable() {
+    }
