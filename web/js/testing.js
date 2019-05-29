b1ee9d0b192601a996adff21474aa8205c7ed7a6
jasonjf <jasonf@reutech.co.za>
2019-05-29 14:52:17 +0200

    Added some table effects

--- web/js/testing.js
+++ web/js/testing.js
@@ -13,9 +13,11 @@ function addRow() {
     for (row; row < 14; row++) {
 
         let newRow = tableRef.insertRow(-1);
-
+        rowLab = ("ANT"+(row+1)+"row");
+        newRow.setAttribute('id',rowLab);
 //        console.log("row" + row);
         for (var cell; cell < 4; cell++) {
+//
 //            console.log(cell);
             lab = ("ANT"+(row+1)+cellLabels[cell])      //row + 1 for antenna offset value
 //            console.log(lab);
@@ -67,3 +69,5 @@ function getPhase(phase_1, phase_2) {
 
     newPhase = Math.abs(phase_1) + Math.abs(phase_2);
     return newPhase;}
+function myOverFunction() {
+    }
